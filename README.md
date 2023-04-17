bb2cdd3c9d8dc7c42a937d89f04258ac42328c14
Rodolfo González González <rodolfo.gonzalez+github@gmail.com>
2023-04-17 01:45:40 -0600

    Source

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # webkit2png-daemon
-Perl daemon which listens for URLs to be screenshoted.
+
+[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+![GitHub all releases](https://img.shields.io/github/downloads/rgglez/webkit2png-daemon) 
+![GitHub issues](https://img.shields.io/github/issues/rgglez/webkit2png-daemon) 
+![GitHub commit activity](https://img.shields.io/github/commit-activity/y/rgglez/webkit2png-daemon)
+
+A daemon written in Perl which listens for URLs to be screenshoted. It uses the [WebKit2PNG](https://pypi.org/project/webkit2png/) script to make the screenshot.
+
+You can configure the listening port, host and other parameters for the daemon using the [Net::Server configuration](https://metacpan.org/pod/Net::Server). 
+The configured port in the script is 15000 for instance.
+
+## Usage
+
+Adjust the paths of the programs here:
+
+```perl
+my $PYTHON = '/usr/bin/python';
+my $XVFB = '/usr/bin/xvfb-run';
+my $WEBKIT2PNG = 'webkit2png.py';
+my $CAT = '/bin/cat';
+``` 
+
+Start the server executing:
+
+```
+perl WebKit2PNGDaemon.pl
+```
+
+Then send a string (the URL to be screenshoted) to the specified port and host. The server will execute WebKit2PNG and return a first line containing
+
+```
+LENGTH: <length of the captured image in bytes>
+```
+
+and then the bytes of the captured screen in PNG format, or an error image.
+
+## Dependencies
+
+### Perl modules 
+
+You can install them using *perl -MCPAN -e shell* or your distro's package manager:
+
+* Net::Server::PreFork
+* GD::Simple
+* File::Temp
+* MIME::Base64
+* Sys::Syslog
+* Digest::SHA
+* Data::Validate::URI
+
+### Programs
+
+* [WebKit2PNG](https://pypi.org/project/webkit2png/)
+* [Python](https://www.python.org/)
+* [xvfb-run](https://github.com/revnode/xvfb-run/blob/master/xvfb-run)
+* [cat](https://man7.org/linux/man-pages/man1/cat.1.html)
+
+## Notes
+
+* You must download the [webkit2png](https://pypi.org/project/webkit2png/) Python script.
+* A simpler and more modern solution to screenshot webpages is provided [here](https://github.com/rgglez/fc-webpage-screenshot).
+
+## TODO
+
+* Use command line parameters and a configuration file.
+
+## License
+
+Copyright (c) 2023, Rodolfo González González.
+
+Read the LICENSE file.
---
+++ src/WebKit2PNGDaemon.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/perl
+
+# BSD 3-Clause License
+# 
+# Copyright (c) 2023, Rodolfo González González
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package MyPackage;
+
+use vars qw(@ISA);
+use Net::Server::PreFork; 
+use GD::Simple;
+use File::Temp qw/ tempfile tempdir /;
+use MIME::Base64;
+use Sys::Syslog;
+use Digest::SHA qw(sha512_hex);
+use Data::Validate::URI qw(is_uri);
+
+@ISA = qw(Net::Server::PreFork);
+
+MyPackage->run(port => 15000);
+exit;
+
+################################################################################
+
+sub errorImg
+{
+   my ($string) = shift;
+
+   my $img = GD::Simple->new(1,1);
+   $img->bgcolor('white');
+   $img->fgcolor('white');
+   #$img->rectangle(0, 0, 250, 200);
+   #$img->moveTo(10, 10);
+   #$img->string($string);
+   my $strImg = $img->png;
+   print "LENGTH: " . length($strImg) . "\n\r";
+   print $strImg;
+}
+
+sub doLog
+{
+   my ($message) = shift;
+
+   openlog("WEBKIT2PNGDAEMON", "ndelay,pid", "local0");
+   syslog('warning', $message . ' %m');
+   closelog();
+}
+
+sub process_request
+{
+   my $self = shift;
+
+   my $PYTHON = '/usr/bin/python';
+   my $XVFB = '/usr/bin/xvfb-run';
+   my $WEBKIT2PNG = 'webkit2png.py';
+   my $CAT = '/bin/cat';
+
+   eval {
+      local $SIG{ALRM} = sub { &errorImg('Timed out.'); };
+      my $timeout = 120;
+
+      my $previous_alarm = alarm($timeout);
+      while (<STDIN>) {
+         s/\r?\n$//;
+         my $url = $_;
+         if (is_uri($url)) {
+            my $file = new File::Temp(UNLINK => 1, DIR => '/tmp');
+            $ENV{'DISPLAY'} = 'localhost:0';
+            my $cmd  = "$XVFB --server-args='-screen 0, 1024x768x24' -a $PYTHON $WEBKIT2PNG -o $file --debug --log=/var/log/webkit2png/webkit2png.log -W -F javascript -F plugins --scale=250 200 --aspect-ratio=crop $url";
+            system($cmd);
+            if (-f $file) {
+               my $filesize = -s $file;
+               my $buffer = 0;
+               print "LENGTH: $filesize\n\r";
+               print `$CAT $file`;
+               &doLog('WEBKIT2PNG: ' . $url);
+            }
+            else {
+               &doLog('Error: Invalid PNG File');
+               &errorImg('Error: Invalid PNG File');
+            }
+         }
+         alarm($timeout);
+      }
+      alarm($previous_alarm);
+   };
+
+   if ($@=~/timed out/i) {
+      &doLog('Timed out.');
+      &errorImg('Timed out.');
+      return;
+   }
+}
+
+1;
