188baaf530e28125ca53c25f45868e3bed7a9324
Rodolfo González González <122424634+rgglez@users.noreply.github.com>
2023-04-19 22:02:23 -0600

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,10 @@
 #!//bin/sh
 
+# You must create an unprivileged user and a group to run the server with.
+# You must create directory for the logs, with the corresponding permissions
+# and ownerships.
+
 rm -f /var/run/webkit.pid
 
-/usr/bin/perl /usr/local/athenea/WebKit2PNGDaemon.pl --host 10.0.1.36 --port 22222 --pid_file=/var/run/webkit.pid --user webkit --group webkit --log_level 4 --log_file /var/log/webkit/daemon.log --background
+/usr/bin/perl /usr/local/bin/WebKit2PNGDaemon.pl --host 127.0.0.1 --port 22222 --pid_file=/var/run/webkit.pid --user webkit --group webkit --log_level 4 --log_file /var/log/webkit/daemon.log --background
 
